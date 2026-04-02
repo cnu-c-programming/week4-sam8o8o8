e9ccbb03af920a329080ec969bfd297da152c0b8
sam8o8o8 <sam87608467@gmail.com>
2026-04-02 23:24:30 +0900

    Update 4-2.c

---
+++ code/4-2/4-2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int sum(int arr[], int n) {
+    int a = 0;
+    for (int i=0; i<n; i++) {
+        a += arr[i];
+    }
+    return a;
+}
+
+double average(int arr[], int n) {
+    int a = 0;
+    for (int i=0; i<n; i++) {
+        a += arr[i];
+    }
+    return (double)a / n;
+}
+
+int max(int arr[], int n) {
+    int a = arr[0];
+    for (int i=0; i<n; i++) {
+        if (arr[i] > a) {
+            a = arr[i];
+        }
+    }
+    return a;
+}
+
+int main() {
+    int arr[] = {3, 7, 1, 9, 4, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("sum: %d\n", sum(arr, n));
+    printf("avg: %.2f\n", average(arr, n));
+    printf("max: %d\n", max(arr, n));
+
+    return 0;
+}
