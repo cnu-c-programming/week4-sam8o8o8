d7b50e288d2bc4197c43b360cadc4017b36f611b
sam8o8o8 <sam87608467@gmail.com>
2026-04-02 23:23:14 +0900

    Update 3-4.c

---
+++ code/3-4/3-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void inc(int* x) {
+    if (x == NULL) {
+        return;
+    }
+    *x = *x + 1;
+    printf("%d\n", *x);
+}
+
+int main() {
+    int x = 3;
+
+    inc(&x);
+    inc(NULL);
+
+    return 0;
+}
